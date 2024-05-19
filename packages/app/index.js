c7361b9e1ee3f06dde8a0c4bc490de24e097a12b
osamaalhabarnah <osama.alhabarnah@seera.sa>
2024-05-19 15:45:45 +0300

    update sotre

--- packages/app/index.js
+++ packages/app/index.js
@@ -1,3 +1,3 @@
-import {store} from 'store';
+import {store} from '@osama.almosafer/store';
 
 console.log(1111, {store})
--- packages/store/index.js
+++ packages/store/index.js
@@ -1,3 +1,3 @@
 export const store = {
-    username: 'user',
+    username: 'yusur',
 }
